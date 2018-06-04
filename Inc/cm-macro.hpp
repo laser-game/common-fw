ad4afb74c6aaa97e8e079c60eeb6aafd9305546a
wykys <vvykys@gmail.com>
2018-06-05 01:55:38 +0200

    added pin_state to int and bool to macro

---
+++ Inc/cm-macro.hpp
@@ -0,0 +1,30 @@
+#ifndef __CM_MACRO_INCLUDED__
+#define __CM_MACRO_INCLUDED__
+
+
+#include <vector>
+
+#ifdef STM32_F0
+# include "stm32f0xx_hal.h"
+#elif defined STM32_F4
+# include "stm32f4xx_hal.h"
+#endif
+
+using namespace std;
+
+#define vector_uint8_t_array(array ...) \
+    ( \
+      vector<uint8_t>({ array }) \
+    )
+
+#define gpio_pin_state_to_int(pin_state) \
+    ( \
+      (pin_state == GPIO_PIN_SET) ? 1 : 0 \
+    )
+
+#define gpio_pin_state_to_bool(pin_state) \
+    ( \
+      (pin_state == GPIO_PIN_SET) ? true : false \
+    )
+
+#endif // ifndef __CM_MACRO_INCLUDED__
