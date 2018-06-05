dc1f574928a9fc3459cdefd4e513013a64759362
wykys <vvykys@gmail.com>
2018-06-06 00:57:26 +0200

    updated uart added byte_array support

--- Inc/cm-uart.hpp
+++ Inc/cm-uart.hpp
@@ -3,7 +3,9 @@
 
 #define UART_TIMEOUT 0xFFFF
 
-#ifdef STM32_F4
+#ifdef STM32_F0
+# include "stm32f0xx_hal.h"
+#elif defined STM32_F4
 # include "stm32f4xx_hal.h"
 #endif
 
@@ -25,35 +27,40 @@ public:
         this->huart = huart;
     }
 
-    inline void set_baudrate(uint32_t baudrate=9600)
+    inline void set_baudrate(uint32_t baudrate = 9600)
     {
         huart->Init.BaudRate = baudrate;
         HAL_UART_Init(huart);
     }
 
-    inline void tx(uint8_t byte)
+    inline HAL_StatusTypeDef tx(uint8_t byte)
     {
-        HAL_UART_Transmit(huart, (uint8_t *) &byte, 1, UART_TIMEOUT);
+        return HAL_UART_Transmit(huart, (uint8_t *) &byte, 1, UART_TIMEOUT);
     }
 
-    inline void tx(vector<uint8_t> vector_bytes)
+    inline HAL_StatusTypeDef tx(uint8_t *byte_array, uint16_t size, uint32_t timeout = UART_TIMEOUT)
     {
-        HAL_UART_Transmit(huart, (uint8_t *) vector_bytes.data(), vector_bytes.size(), UART_TIMEOUT);
+        return HAL_UART_Transmit(huart, byte_array, size, timeout);
     }
 
-    inline void tx(string str)
+    inline HAL_StatusTypeDef tx(vector<uint8_t> vector_bytes)
     {
-        HAL_UART_Transmit(huart, (uint8_t *) str.c_str(), str.size(), UART_TIMEOUT);
+        return HAL_UART_Transmit(huart, (uint8_t *) vector_bytes.data(), vector_bytes.size(), UART_TIMEOUT);
     }
 
-    inline void rx(uint8_t *p_data, uint16_t size, uint32_t timeout = 10)
+    inline HAL_StatusTypeDef tx(string str)
     {
-        HAL_UART_Receive(huart, p_data, size, timeout);
+        return HAL_UART_Transmit(huart, (uint8_t *) str.c_str(), str.size(), UART_TIMEOUT);
     }
 
-    inline void rx_it(void)
+    inline HAL_StatusTypeDef rx(uint8_t *p_data, uint16_t size, uint32_t timeout = UART_TIMEOUT)
     {
-        HAL_UART_Receive_IT(huart, buffer_rx, 1);
+        return HAL_UART_Receive(huart, p_data, size, timeout);
+    }
+
+    inline HAL_StatusTypeDef rx_it(void)
+    {
+        return HAL_UART_Receive_IT(huart, buffer_rx, 1);
     }
 
     inline UART_HandleTypeDef * get_huart()
